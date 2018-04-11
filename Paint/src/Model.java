2ab5c09d864f366bba5386c8a4910a9a14a5ba79
Andreas Haglund <andh16@uia.no>
2018-04-11 12:50:49 +0200

    Added drawLine function

--- Paint/src/Model.java
+++ Paint/src/Model.java
@@ -1,31 +1,16 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
 
-public class Model {
-    View myView;
-    JFileChooser fileChooser = new JFileChooser();
+public class Model extends JComponent {
 
-    //Open image stream
 
-    public JLabel openFile () throws IOException {
-        fileChooser.showOpenDialog(myView);
-        FileInputStream fileStream = new FileInputStream(fileChooser.getSelectedFile().getAbsolutePath());
-        BufferedInputStream bufferedStream = new BufferedInputStream(fileStream);
-        Image img = ImageIO.read(bufferedStream);
-        ImageIcon imgIcon = new ImageIcon(img);
-        JLabel label = new JLabel(imgIcon);
-        bufferedStream.close();
-        fileStream.close();
-        return label;
+    public void paint (Graphics g, Point pointStart, Point pointEnd) {
+        if (pointStart != null) {
+            g.setColor(Color.black);
+            g.drawLine(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y);
+        }
     }
-
-    public void saveFile(JPanel jpanel){
-        fileChooser.showSaveDialog(myView);
-
-    }
-    public void straightLine(){
+    public void openFile (JPanel jPanel ) {
 
     }
 }
--- Paint/src/View.java
+++ Paint/src/View.java
@@ -1,56 +1,95 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.*;       // Using AWT's Graphics and Color
-import java.awt.event.*; // Using AWT event classes and listener interfaces
-import javax.swing.*;
-
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class View extends JFrame {
 
+    // Globale Variabler
     private static final int CANVAS_WIDTH = 800;
     private static final int CANVAS_HEIGT = 700;
+    private static final int STATUSBAR_HEIGHT = 20;
+    private DrawCanvas canvas = new DrawCanvas();
 
-    JPanel mainPanel = new JPanel();
+    // Meny
     JMenuBar menuBar = new JMenuBar();
     JMenu menu = new JMenu("Fil");
-    JButton drawLine = new JButton("Rett linje");
+
+    // Panel where the straight line is drawn, and button to activate it
     JPanel mousePanel = new JPanel();
-    JPanel statusPanel = new JPanel();
-    JLabel statusLabel = new JLabel("Status");
+    JButton drawLine = new JButton("Rett linje");
+
+    // Knapper tilhørende
     JMenuItem aapne = new JMenuItem("Åpne");
     JMenuItem lagre = new JMenuItem("Lagre");
     JMenuItem lagreSom = new JMenuItem("Lagre som...");
 
+    // Knapper tilhørednde menybaren.
+    JButton tegne = new JButton("Tegn");
+
+    // Statuspanel (nederst)
+    JPanel statusPanel = new JPanel();
+    JLabel statusBar = new JLabel("Status");
+
+
     public View (Controller controller) {
 
-        this.add(mainPanel);
+        super("Tittel");
+        this.setLayout(new BorderLayout());
+        this.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGT));
+
+        //Container cp = getContentPane();
+
+        this.add(canvas, BorderLayout.CENTER);
+        this.add(mousePanel, BorderLayout.CENTER);
+        pack(); //??????
+
+        // statusBar
+        statusPanel.setBorder((new BevelBorder(BevelBorder.LOWERED)));
+        this.add(statusPanel, BorderLayout.SOUTH);
+        statusPanel.setPreferredSize(new Dimension(this.getWidth(), STATUSBAR_HEIGHT));
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+        statusBar.setHorizontalAlignment(SwingConstants.LEFT);
+        statusPanel.add(statusBar);
+
+        // Add buttons to menubar
         menu.add(aapne);
         menu.add(lagre);
         menu.add(lagreSom);
         menuBar.add(menu);
+        menuBar.add(tegne);
         menuBar.add(drawLine);
-        this.add(menuBar);
+        this.setJMenuBar(menuBar);
 
+        // Add action to buttons
         aapne.addActionListener(controller);
         lagre.addActionListener(controller);
         lagreSom.addActionListener(controller);
         drawLine.addActionListener(controller);
 
-        this.setJMenuBar(menuBar);
-        this.setLayout(new BorderLayout());
+        canvas.addMouseListener(controller);
+        canvas.addMouseMotionListener(controller);
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(CANVAS_WIDTH,CANVAS_HEIGT);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        this.add(mousePanel, BorderLayout.CENTER);
+    }
 
-        class Canvas extends JPanel {
 
+    // Panelet som alle tegningene gjøres opp på.
+    private class DrawCanvas extends JPanel {
+
+        DrawCanvas() {
+            this.setBackground(Color.white);
         }
 
+
+        @Override
+        public void paintComponent (Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.WHITE);
+
+        }
     }
 
 
